b9cda61acbf0e588f39abb7d62e54842147977ce
Javier Ortega Aguayo <jaortega2021@udec.cl>
2025-03-19 18:11:22 -0300

    Clase1_v1
    En esta versión nosotros aprendimos el print

---
+++ Clase1.py
@@ -0,0 +1,11 @@
+# Comentario 1
+#* Comentario 2
+#! Comentario 3
+#? Comentario 4
+
+#! De que se trata el script, un resumen, por ejemplo: Este script corresponde a la primera clase de la asignatura de python
+
+#? p = 2.7
+#? pcu = 4.5
+
+print("Hola mundo") 
