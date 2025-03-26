3d4ff208075e89b4a7bb5111fa9cc9fe6b937d6a
JavierOrtegaAguayo <jaortega2021@udec.cl>
2025-03-26 17:58:37 -0300

    Primer commit

--- Clase_03_26.py
+++ Clase_03_26.py
@@ -1,1 +1,15 @@
-print("Hola mundo")
+# Tipos de variable
+num = 30
+nombre = "Javier"
+num1, num2 = 10, 20
+grav = 9.8
+
+my_lista = [1, 2, 9.8, "Hola", 1000, [1, 2]]
+my_tupla = (1, "Chao", 20.4, ["a", "b", 3], 90)
+
+dicc = {}
+
+numero = 20
+
+if numero == 20:
+    print("Numero es igual a 20")
